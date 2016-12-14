bb02d8d523f463f3f6611673b7023f7ec19e92b9
Glassmaker <JeanGlassmaker@mail.com>
2016-12-14 21:00:46 +0300

    Fixes papyrus in world rendering

--- src/main/java/com/teammetallurgy/atum/blocks/BlockPapyrus.java
+++ src/main/java/com/teammetallurgy/atum/blocks/BlockPapyrus.java
@@ -3,8 +3,13 @@ package com.teammetallurgy.atum.blocks;
 import com.teammetallurgy.atum.items.AtumItems;
 import net.minecraft.block.BlockReed;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
@@ -16,12 +21,41 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.Random;
 
-public class BlockPapyrus extends BlockReed {
+public class BlockPapyrus extends BlockReed implements IAtumBlock {
 
+	public static final PropertyBool TOP = PropertyBool.create("top");
+			
     public BlockPapyrus() {
         super();
         this.setHardness(0.0F);
-        this.setSoundType(SoundType.GLASS);
+        this.setSoundType(SoundType.PLANT);
+    }
+    
+
+    @Override
+    public Class<? extends ItemBlock> getItemClass() {
+        return ItemBlock.class;
+    }
+
+    @Override
+    public IProperty[] getNonRenderingProperties() {
+        return new IProperty[]{AGE};
+    }
+
+    @Override
+    public String getStateName(IBlockState state) {
+        return "";
+    }
+    
+    @Override
+    protected BlockStateContainer createBlockState() {
+    	return new BlockStateContainer(this, new IProperty[] {AGE, TOP});
+    }
+    
+    @Override
+    public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos) {
+    	BlockPos upperPos = pos.add(0, 1, 0);
+		return state.withProperty(TOP, world.isAirBlock(upperPos));
     }
 
     @Override
